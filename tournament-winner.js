e3d8b4bfd73c963c1b92515c6a8c9ecf4c5ac82c
Jamie Benjamin <bennythedev@gmail.com>
2023-07-10 16:02:19 -0500

    tournament winner pt 2

--- tournament-winner.js
+++ tournament-winner.js
@@ -14,3 +14,20 @@ const tournamentWinner = (competitions, results) => {
 }
 
 tournamentWinner([["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]], [0, 0, 1])
+
+// const tournamentWinner = (competitions, results) => {
+//   let score = {};
+//   competitions.map((competition, index) => {
+//     let hometeam = competition[0];
+//     let awayteam = competition[1];
+//     if (results[index] === 0) {
+//       score[awayteam] ? score[awayteam] +=3 : score[awayteam]= 3;
+//     } else {
+//       score[hometeam] ? score[hometeam] +=3 : score[hometeam] = 3;
+//     }
+//   })
+//   let winner = Object.entries(score).sort((a, b) => b[1] - a[1])[0][0]
+//   return winner
+// }
+
+// tournamentWinner([["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]], [0, 0, 1])
