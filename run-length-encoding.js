8547b7464d2548c0fe7d1184f4db0aba94eae80a
Jamie Benjamin <bennythedev@gmail.com>
2023-07-15 23:37:07 -0500

    run length solution

--- run-length-encoding.js
+++ run-length-encoding.js
@@ -1,13 +1,15 @@
-const runLengthEncoding = (string) => {
+function runLengthEncoding(string) {
   let outputString = ""
-  let counter = 1;
-  let i = 0;
-  while (i < string.length) {
-    outputString = outputString.concat(counter, string[i]);
-    i = counter + i;
-    counter = 1;
+  let count = 1;
+  
+  for (let i = 0; i < string.length; i++) {
+    count++
+    if (string[i] !== string[i + 1] || count === 9) {
+      outputString = outputString + string[i] + count;
+      count = 0;
+    }
   }
   return outputString;
 }
 
-runLengthEncoding("AAAAAAAAABBCCCCDD")
+runLengthEncoding("AAAAAAAAAAAABBCCCCDD")
